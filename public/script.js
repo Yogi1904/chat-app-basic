9cbde37f7afdfc329b70505c0a614edf9d5bd74f
Yogi1904 <teg.yogendraasure@gmail.com>
2026-03-03 06:44:50 +0530

    Extra changes made - to UI and functionality

--- public/script.js
+++ public/script.js
@@ -1,21 +1,56 @@
-const socket = io();
-console.log('client connected')
-
 const sendBtn = document.getElementById('sendBtn')
 const messageInput = document.getElementById('message')
 const allMessages = document.getElementById('messages')
 
+// const username = "yogi"
+const username = prompt('Enter your username: ')
 
-socket.on('message', (message) => {
+const socket = io();
+console.log('client connected')
+socket.emit('user-joined', username)
+
+
+socket.on('message', ({username, message, time}) => {
     const p = document.createElement('p')
-    p.innerText = message
+    p.innerText = `(${time}) ${username}: ${message}`
+    p.classList.add("mb-2")
     allMessages.appendChild(p)
+    allMessages.scrollTop = allMessages.scrollHeight
+})
+
+socket.on('joined', (username) => {
+    const p = document.createElement('p')
+    p.innerText = username + " has joined the session!"
+    p.classList.add("text-secondary",  "small")
+    allMessages.appendChild(p)
+    allMessages.scrollTop = allMessages.scrollHeight
+})
+
+socket.on('disconnected', (username) => {
+    const p = document.createElement('p')
+    p.innerText = username + " left the session!"
+    p.classList.add("text-secondary",  "small")
+    allMessages.appendChild(p)
+    allMessages.scrollTop = allMessages.scrollHeight
+})
+
+messageInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        sendBtn.click()
+    }
 })
 
 sendBtn.addEventListener('click', (e) => {
     const message = messageInput.value
+    const time = timeExtractor()
     if(message) {
         // console.log('sent by you!')
-        socket.emit('user-message', message)
+        socket.emit('user-message', {username: username, message: message, time: time})
+        messageInput.value = ""
     }
 })
+
+function timeExtractor(){
+    const now = new Date()
+    return now.toTimeString().slice(0, 5)
+}
--- server.js
+++ server.js
@@ -9,16 +9,26 @@ const io = new Server(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id)
-})
+//making bootstrap available for use
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')))
 
-io.on("connection", (socket) => {
-    socket.on("user-message", (message) => {
-        // console.log('a new message:', message)
-        io.emit('message', message)
+io.on('connection', (socket) => {
+    console.log('User connected.', socket.id)
+
+    socket.on("user-joined", (username) => {
+        socket.username = username
+        io.emit('joined', username)
+    })
+
+    socket.on("user-message", ({username, message, time}) => {
+        io.emit('message', {username, message, time})
+    })
+
+    socket.on("disconnect", () => {
+        io.emit('disconnected', socket.username)
     })
 })
+
 
 server.listen(8080, () => {
     console.log('Listening on port 8080')
